6f8b113b8526df831d4d4d2b1a9bb445d970f055
chisimdi <chisimdi.ezeanieto@nayalabs.co>
2021-09-19 10:26:33 +0100

    sending message both from the client and server.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const http = require('http');
+const webSocket = require('websocket');
 
 const PORT = process.env.PORT || 8090;
 
+const WebSocketServer = webSocket.server;
+let connection = null;
+
 const httpServer = http.createServer((req, res) => {
   console.log('We have received a call');
+});
+
+const ws = new WebSocketServer({
+  "httpServer": httpServer
+});
+
+ws.on('request', req => {
+  connection = req.accept(null, req.origin);
+  connection.on('open', () => console.log('Connection has been opened'));
+  connection.on('close', () => console.log('Connection has been closed'));
+  connection.on('message', (message) => {
+    console.log(`Received message ${message.utf8Data}`)
+  });
 });
 
 httpServer.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
